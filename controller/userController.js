559302ad00c4ae968dd9d371cbdcfb273a5f1f72
Shubham <shubhamkr1177@gmail.com>
2024-08-19 17:43:42 +0530

    Referenced user with smtp

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,13 @@
 const User = require('../Models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+// THESE ARE ALL ADMIN FUNCTIONS
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   res.status(200).json({
@@ -12,11 +19,6 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getMe = (req, res, next) => {
-  req.params.id = req.user.id;
-  next();
-};
-
 exports.getUser = catchAsync(async (req, res, next) => {
   const currentUser = await User.findById(req.params.id);
   if (!currentUser)
@@ -26,5 +28,28 @@ exports.getUser = catchAsync(async (req, res, next) => {
     data: {
       currentUser,
     },
+  });
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const user = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!user) return next(new AppError('No User found with that id', 404));
+  res.status(200).json({
+    status: 'success',
+    data: {
+      doc,
+    },
+  });
+});
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  const user = await Model.findByIdAndDelete(req.params.id);
+  if (!user) return next(new AppError('No User found with that id', 404));
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
